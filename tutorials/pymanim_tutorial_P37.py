8e2a147fa03ded80d53fe95edaec671c6b62ee97
ishaandey <ishaan.dey8@gmail.com>
2020-04-24 12:58:02 -0400

    Coriolis

--- tutorials/pymanim_tutorial_P37.py
+++ tutorials/pymanim_tutorial_P37.py
@@ -13,7 +13,8 @@ class Shapes(Scene):
         line=Line(np.array([3,0,0]),np.array([5,0,0]))
         triangle=Polygon(np.array([0,0,0]),np.array([1,1,0]),np.array([1,-1,0]))
 
-        
+        self.add(circle)
+        self.play(ShowCreation(line))
         self.play(ShowCreation(circle))
         self.play(FadeOut(circle))
         self.play(GrowFromCenter(square))
